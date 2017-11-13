07d439c2ab80a5044b451a169cbc2ebc090fd529
Satyajit Sahoo <satyajit.happy@gmail.com>
2017-11-13 15:03:10 +0530

    fix: add babel plugin for deterministic class names

--- src/lib/styling.js
+++ src/lib/styling.js
@@ -5,20 +5,28 @@ import hash from 'short-hash';
 
 let sheet = '';
 
-export function dump() {
-  return sheet;
-}
-
-export function css(template: Array<string>, ...expressions: Array<string>) {
+const named = (name?: string = 'css', filename: ?string = null) => (
+  template: Array<string>,
+  ...expressions: Array<string>
+) => {
   const styles = template.reduce(
     (accumulator, part, i) => accumulator + expressions[i - 1] + part
   );
 
-  const slug = `css-${hash(styles)}`;
-  const selector = `.${slug}`;
+  const slug = hash(styles || filename);
+  const classname = `${name}__${slug}`;
+  const selector = `.${classname}`;
   const rules = stylis(selector, styles);
 
   sheet += `${rules}\n`;
 
-  return slug;
+  return classname;
+};
+
+export const css = named();
+
+css.named = named;
+
+export function dump() {
+  return sheet;
 }

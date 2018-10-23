09a8e732c3f86ecce9627e635c8009b1e4bb3edc
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-10-24 01:50:20 +0200

    refactor: replace remarkable with markdown-it

--- src/templates/Markdown.js
+++ src/templates/Markdown.js
@@ -2,7 +2,7 @@
 
 import * as React from 'react';
 import { css, cx } from 'linaria';
-import Remarkable from 'remarkable';
+import MarkdownIt from 'markdown-it';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
@@ -89,7 +89,7 @@ type Props = {
 
 export default class Markdown extends React.Component<Props> {
   render() {
-    const md = new Remarkable({
+    const md = new MarkdownIt({
       linkify: true,
       html: true,
       highlight: (code, lang) => {

887d7ce7495e03dd20d16e6169cb6a34c8b74e9a
Satyajit Sahoo <satyajit.happy@gmail.com>
2018-12-02 14:59:05 +0100

    refactor: switch to marked for markdown

--- src/templates/Markdown.js
+++ src/templates/Markdown.js
@@ -2,8 +2,9 @@
 
 import * as React from 'react';
 import { css, cx } from 'linaria';
-import MarkdownIt from 'markdown-it';
-import headings from 'markdown-it-github-headings';
+import marked from 'marked';
+import sanitize from 'sanitize-html';
+import escape from 'escape-html';
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
@@ -13,7 +14,7 @@ import 'prismjs/components/prism-jsx';
 const markdown = css`
   .anchor {
     margin-left: -20px;
-    padding-right: 4px;
+    margin-right: -2px;
     opacity: 0;
 
     &:hover {
@@ -108,22 +109,44 @@ type Props = {
   className?: string,
 };
 
+const renderer = new marked.Renderer();
+
+renderer.heading = function heading(...args) {
+  return marked.Renderer.prototype.heading.apply(this, args).replace(
+    /^(<h[1-3] .+>)(.+)(<\/h[1-3]>)/,
+    (match, p1, p2, p3) =>
+      `${p1}
+          <a class="anchor" href="#${p2
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/, '-')}">
+            <svg viewBox="0 0 16 16" width="16" height="16" aria-hidden="true">
+              <path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path>
+            </svg>
+          </a>
+        ${p2}${p3}`
+  );
+};
+
 export default class Markdown extends React.Component<Props> {
   render() {
-    const md = new MarkdownIt({
-      linkify: true,
-      html: true,
+    let html = marked(this.props.source, {
+      renderer,
+      gfm: true,
+      silent: true,
       highlight: (code, lang) => {
-        const language = lang === 'js' ? languages.jsx : languages[lang];
-        return language ? highlight(code, language) : null;
+        const grammar = lang === 'js' ? languages.jsx : languages[lang];
+        return grammar ? highlight(code, grammar) : escape(code);
       },
-    }).use(headings);
+    });
+
+    // eslint-disable-next-line global-require
+    html = sanitize(html, require('../configs/santize-config.json'));
 
     return (
       <div
         // eslint-disable-next-line react/no-danger
         dangerouslySetInnerHTML={{
-          __html: md.render(this.props.source),
+          __html: html,
         }}
         className={cx(this.props.className, markdown)}
       />

80dcd166299e38f27dcbc67e3ba2614fd80a3e63
jbinda <jakub.binda@gmail.com>
2020-05-24 20:13:41 +0200

    feat: export Header and ToggleTheme component to allow use in js (#39)
    fixes #38 
    
    Add export statement for `Header` and `ThemeToggle` components to allow direct use in react-native-paper `.js` docs sections (Home and Showcase)

--- components.js
+++ components.js
@@ -1,3 +1,5 @@
 /* @flow */
 
 exports.Link = require('./dist/templates/Link').default;
+exports.ThemeToggle = require('./dist/templates/ThemeToggle').default;
+exports.Header = require('./dist/templates/Header').default;
